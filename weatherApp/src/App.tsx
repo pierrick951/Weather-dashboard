46601c7e1d50862de859614b80cd6ea5fba4709f
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-06-28 15:38:00 -0400

    start weather dashboard

---
+++ weatherApp/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+const WEATHERKEY = import.meta.env.VITE_WEATHER_API_KEY
+
+
+type Props = {}
+export default function App({}: Props) {
+
+  return (
+
+    <div>App</div>
+    
+  )
+}
